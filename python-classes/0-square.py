61560334b4f3dd35a5e750a37e6f1fa396a2c3d0
zehramans <zehramansirova0@gmail.com>
2025-11-22 10:07:24 -0500

    changes saved

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python3"
-""" 
+"""
 Is it okay if i add random stuff here
 """
+
+
 class Square:
     """
     and here
     """
+
     def __init__(self, size):
-        """ 
+        """
         and here
         """
         self.__size = size
