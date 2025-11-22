ed7846883a1cbb023d0430ef0dae427580a7e454
zehramans <zehramansirova0@gmail.com>
2025-11-22 10:16:06 -0500

    changes saved

---
+++ python-classes/1-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3"
+"""
+Is it okay if i add random stuff here
+"""
+
+
+class Square:
+    """
+    and here
+    """
+
+    def __init__(self, size):
+        """
+        and here
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
